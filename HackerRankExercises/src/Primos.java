703abb635d7b6a78fbc9791a6a41b4fed7a50c1c
lizeth9797 <karinaavao@gmail.com>
2024-03-09 20:13:23 -0600

    se sube solución a numeros primos-Ejercicio HackerRank

--- HackerRankExercises/src/Primos.java
+++ HackerRankExercises/src/Primos.java
@@ -4,13 +4,35 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 import java.lang.reflect.*;
-
-
+//https://www.hackerrank.com/challenges/prime-checker/problem?isFullScreen=true
+	class Prime{
+		public void checkPrime(int ... numbers) { //metodo dentro de la clase Prime para determinar primos
+			for (int i : numbers) { 
+				boolean prime=true;
+				if(i>1) {
+					//modulo para saber si es divisible por otro num diferente a el mismo o 1
+					for(int j=(i-1);j>1;j--) { 
+						if(i%j==0) { 
+							prime=false;
+							break;
+						}
+					}
+				}else {
+					prime=false;
+				}
+				if(prime) {
+					System.out.print(i+" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	
 public class Primos {
-
 	public static void main(String[] args) {
 		try{
-		BufferedReader br=new BufferedReader(new InputStreamReader(in));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n1=Integer.parseInt(br.readLine());
 		int n2=Integer.parseInt(br.readLine());
 		int n3=Integer.parseInt(br.readLine());
@@ -21,6 +43,7 @@ public class Primos {
 		ob.checkPrime(n1,n2);
 		ob.checkPrime(n1,n2,n3);
 		ob.checkPrime(n1,n2,n3,n4,n5);	
+		//revisa si hay sobrecarga de metodos:
 		Method[] methods=Prime.class.getDeclaredMethods();
 		Set<String> set=new HashSet<>();
 		boolean overload=false;
